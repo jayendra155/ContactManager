eebae751ef2233bf62a7278c7ce7a9bb6e610f26
Jayendra Vikram Singh <jayendra155@hotmail.com>
2016-06-06 16:07:43 +0530

    svg files added and menu icon used

--- dist/boot.js
+++ dist/boot.js
@@ -1,8 +1,12 @@
 ///  <reference path="_all.ts" />
 var ContactManagerApp;
 (function (ContactManagerApp) {
-    angular.module("contactManagerApp", ['ngMaterial'])
-        .service('userService', ContactManagerApp.UserService);
-    angular.module('contactManagerApp').controller('mainController', ContactManagerApp.MainController);
+    angular.module("contactManagerApp", ['ngMaterial', 'ngMdIcons'])
+        .service('userService', ContactManagerApp.UserService)
+        .controller('mainController', ContactManagerApp.MainController)
+        .config(function ($mdIconProvider) {
+        $mdIconProvider.icon('menu', './assets/svg/menu.svg', 24);
+        $mdIconProvider.icon('sidenav_left', './assets/svg/sidenav_left.svg', 24);
+    });
 })(ContactManagerApp || (ContactManagerApp = {}));
 //# sourceMappingURL=boot.js.map
--- src/boot.ts
+++ src/boot.ts
@@ -1,6 +1,10 @@
 ///  <reference path="_all.ts" />
 module ContactManagerApp {
-    angular.module("contactManagerApp", ['ngMaterial'])
-        .service('userService', UserService);
-        angular.module('contactManagerApp').controller('mainController', MainController);
+    angular.module("contactManagerApp", ['ngMaterial' , 'ngMdIcons'])
+        .service('userService', UserService)
+        .controller('mainController', MainController)
+        .config(($mdIconProvider: angular.material.IIconProvider)=>{
+            $mdIconProvider.icon('menu','./assets/svg/menu.svg', 24);
+            $mdIconProvider.icon('sidenav_left','./assets/svg/sidenav_left.svg', 24);
+        });
 }
