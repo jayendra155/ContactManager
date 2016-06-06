5cc4dbda9113d21ed96952970a92ef2e7558b875
Jayendra Vikram Singh <jayendra155@hotmail.com>
2016-06-06 16:15:18 +0530

    theme configured

--- dist/boot.js
+++ dist/boot.js
@@ -4,9 +4,12 @@ var ContactManagerApp;
     angular.module("contactManagerApp", ['ngMaterial', 'ngMdIcons'])
         .service('userService', ContactManagerApp.UserService)
         .controller('mainController', ContactManagerApp.MainController)
-        .config(function ($mdIconProvider) {
+        .config(function ($mdIconProvider, $mdThemingProvider) {
         $mdIconProvider.icon('menu', './assets/svg/menu.svg', 24);
         $mdIconProvider.icon('sidenav_left', './assets/svg/sidenav_left.svg', 24);
+        $mdThemingProvider.theme('default')
+            .primaryPalette('grey')
+            .accentPalette('red');
     });
 })(ContactManagerApp || (ContactManagerApp = {}));
 //# sourceMappingURL=boot.js.map
--- src/boot.ts
+++ src/boot.ts
@@ -3,8 +3,15 @@ module ContactManagerApp {
     angular.module("contactManagerApp", ['ngMaterial' , 'ngMdIcons'])
         .service('userService', UserService)
         .controller('mainController', MainController)
-        .config(($mdIconProvider: angular.material.IIconProvider)=>{
+        .config(($mdIconProvider: angular.material.IIconProvider,
+                $mdThemingProvider: angular.material.IThemingProvider)=>{
+                    
             $mdIconProvider.icon('menu','./assets/svg/menu.svg', 24);
             $mdIconProvider.icon('sidenav_left','./assets/svg/sidenav_left.svg', 24);
+            
+            $mdThemingProvider.theme('default')
+                                .primaryPalette('grey')
+                                .accentPalette('red');
+            
         });
 }
