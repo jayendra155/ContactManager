5e91162c2a4a86ae3535111d379694a946ffba28
Jayendra Vikram Singh <jayendra155@hotmail.com>
2016-06-06 12:56:39 +0530

    Application setup complete

---
+++ dist/controllers/mainController.js
@@ -0,0 +1,13 @@
+///  <reference path="../_all.ts" />
+var ContactManagerApp;
+(function (ContactManagerApp) {
+    var MainController = (function () {
+        function MainController() {
+            this.message = "Hello from Controller";
+        }
+        MainController.$inject = [];
+        return MainController;
+    }());
+    ContactManagerApp.MainController = MainController;
+})(ContactManagerApp || (ContactManagerApp = {}));
+//# sourceMappingURL=mainController.js.map
---
+++ src/controllers/mainController.ts
@@ -0,0 +1,10 @@
+///  <reference path="../_all.ts" />
+module ContactManagerApp {
+    export class MainController {
+        static $inject = [];
+        constructor() {
+
+        }
+        message: string = "Hello from Controller";
+    }
+}
