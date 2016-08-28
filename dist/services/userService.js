397886d6f4cf5d338296b48ef685430c7a715022
jayendra155 <jayendra155@hotmail.com>
2016-08-28 18:40:57 +0530

    Details for Ankit Verma and Piyush updated

--- dist/services/userService.js
+++ dist/services/userService.js
@@ -11,8 +11,8 @@ var ContactManagerApp;
                     avatar: 'svg-3',
                     bio: 'I am an Information Science Engineer who loves to code, play cricket and football',
                     notes: [
-                        { title: 'Behas karwa lo', date: new Date("2016-06-06") },
-                        { title: 'Bakwaas karwa lo', date: new Date("2016-02-14") }
+                        { title: 'Payback Dinner', date: new Date("2016-06-06") },
+                        { title: 'Buy something', date: new Date("2016-02-14") }
                     ]
                 },
                 {
@@ -20,15 +20,17 @@ var ContactManagerApp;
                     avatar: 'svg-2',
                     bio: 'I am an Information Science Engineer who loves to code',
                     notes: [
-                        { title: 'Makkari karni hai', date: new Date("2016-06-06") },
-                        { title: 'RESTful self services banani hain', date: new Date("2016-02-14") }
+                        { title: 'Payback Dinner', date: new Date("2016-06-06") },
+                        { title: 'Buy something', date: new Date("2016-02-14") }
                     ]
                 },
                 {
                     name: 'Chuhaad Singhal',
                     avatar: 'svg-1',
                     bio: 'I am a Computer Science Engineer who loves to be sad',
-                    notes: []
+                    notes: [
+                        { title: 'They see me rollin\' they hatin\' ', date: new Date("1991-07-31") }
+                    ]
                 },
                 {
                     name: 'Chuddu Pagla',
@@ -39,7 +41,7 @@ var ContactManagerApp;
                         { title: 'Nasha kar lo', date: new Date("2016-02-14") }
                     ]
                 },
-                 {
+                {
                     name: 'Takla',
                     avatar: 'svg-1',
                     bio: 'I am an Information Science Engineer who loves to do anything esp parties and nighouts on weekdays',
